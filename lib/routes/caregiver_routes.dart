25b5994ab3b726b9848caf20d32a8d2ff4e67f0e
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-20 03:54:28 +0530

    giver routes

---
+++ lib/routes/caregiver_routes.dart
@@ -0,0 +1,8 @@
+import 'package:carely/features/care_giver/screens/registration_cg.dart';
+import 'package:flutter/material.dart';
+
+
+final Map<String, WidgetBuilder> caregiverRoutes = {
+  // '/provider/home': (context) => const ProviderHomeScreen(),
+  '/caregiver/register': (context) =>  CaregiverRegistrationScreen(),
+};
