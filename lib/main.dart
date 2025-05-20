cfe70100d1856d8863c876551f4fa8a7e8bdf7b2
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-20 15:18:18 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Carely',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-       initialRoute: '/caregiver/register', // '/seeker/main',
+       initialRoute: '/caregiver/main', // '/seeker/main',
       onGenerateRoute: AppRouter.generateRoute,
     );
   }
