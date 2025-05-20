2aa742074dd35681496e696ab08f3a4ac64d83af
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-20 15:18:14 +0530

    caregicer routes

--- lib/routes/caregiver_routes.dart
+++ lib/routes/caregiver_routes.dart
@@ -1,8 +1,11 @@
+import 'package:carely/features/care_giver/screens/home_screen_cg.dart';
+import 'package:carely/features/care_giver/screens/main_screen_ss.dart';
 import 'package:carely/features/care_giver/screens/registration_cg.dart';
 import 'package:flutter/material.dart';
 
 
 final Map<String, WidgetBuilder> caregiverRoutes = {
-  // '/provider/home': (context) => const ProviderHomeScreen(),
+  '/caregiver/main': (context) => const MainScreenCg(),
   '/caregiver/register': (context) =>  CaregiverRegistrationScreen(),
+  '/caregiver/home': (context) =>  HomeScreenCg(),
 };
