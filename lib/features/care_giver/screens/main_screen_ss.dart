461d1a545cb98688e4bfae9fdb89e9b237c5c748
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-20 15:19:21 +0530

    main screen SS

---
+++ lib/features/care_giver/screens/main_screen_ss.dart
@@ -0,0 +1,46 @@
+import 'package:carely/features/care_giver/screens/bookings_screen_cg.dart';
+import 'package:carely/features/care_giver/screens/home_screen_cg.dart';
+import 'package:carely/features/service_seeker/screens/bookings_screen_ss.dart';
+import 'package:carely/features/service_seeker/screens/home_screen_ss.dart';
+import 'package:carely/features/service_seeker/screens/profile_scree_ss.dart';
+import 'package:carely/features/service_seeker/widgets/bottom_nav_bar_ss.dart';
+import 'package:flutter/material.dart';
+
+class MainScreenCg extends StatefulWidget {
+  const MainScreenCg({super.key});
+
+  @override
+  MainScreenCgState createState() => MainScreenCgState();
+}
+
+class MainScreenCgState extends State<MainScreenCg> {
+  int _currentIndex = 0;
+  
+  final List<Widget> _screens = [
+    
+    
+    // ChatScreen(),
+    // ProfileScreen(),
+    HomeScreenCg(),
+    BookingsScreenCg(),
+    Center(child: Text('Chat Screen')),
+    Center(child: Text('Chat Screen')),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavBarSs(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+      ),
+    );
+  }
+}
