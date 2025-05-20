9ddfbe2b9e7c3ceeb7998d49f2e85249167b8ff1
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-05-20 15:17:53 +0530

    booking model (second phase)

--- lib/models/booking_model.dart
+++ lib/models/booking_model.dart
@@ -1,9 +1,5 @@
-enum BookingStatusType {
-  confirmed,
-  pending,
-  cancelled,
-  completed,
-}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+enum BookingStatusType { confirmed, pending, cancelled, completed }
 
 class Booking {
   final String id;
@@ -11,10 +7,18 @@ class Booking {
   final String caregiverName;
   final double caregiverRating;
   final String caregiverImageUrl;
+
+  final String clientId;
+  final String clientName;
+  final String clientImageUrl;
+  final String clientAddress;
+
+  final String careType;
   final DateTime date;
   final String timeStart;
   final String timeEnd;
-  final String location;
+  final String? notes;
+
   final BookingStatusType status;
 
   Booking({
@@ -23,10 +27,15 @@ class Booking {
     required this.caregiverName,
     required this.caregiverRating,
     required this.caregiverImageUrl,
+    required this.clientId,
+    required this.clientName,
+    required this.clientImageUrl,
+    required this.clientAddress,
+    required this.careType,
     required this.date,
     required this.timeStart,
     required this.timeEnd,
-    required this.location,
+    this.notes,
     required this.status,
   });
 }
